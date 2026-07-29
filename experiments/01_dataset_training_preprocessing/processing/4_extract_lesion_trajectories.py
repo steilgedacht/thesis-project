2b976d64d9a339936a0fcba98147bc8de17e93b5
Benjamin Bergmann <7benjaminbergmann@gmail.com>
2026-07-29 13:20:55 +0200

    rerunning all the lesion extractions

--- experiments/01_dataset_training_preprocessing/processing/4_extract_lesion_trajectories.py
+++ experiments/01_dataset_training_preprocessing/processing/4_extract_lesion_trajectories.py
@@ -1,16 +1,22 @@
-import glob
 import tqdm as tqdm
 from concurrent.futures import ThreadPoolExecutor, as_completed
-from mri_dataloader import Patient, MRI_Dataloader
+import sys
+sys.path.insert(1, '.')
+
+from utils.patient import Patient
+from utils.mri_dataloader import MRI_Dataloader
+
+import gc
 
 def process_and_save(input_path):
     patient = Patient(input_path)
     patient.merge_lesion_to_trajectory()
+    gc.collect()
 
 if __name__ == "__main__":
     data_loader = MRI_Dataloader()
 
-    with ThreadPoolExecutor(max_workers=6) as executor:
+    with ThreadPoolExecutor(max_workers=1) as executor:
         futures = {executor.submit(process_and_save, patient): patient for patient in data_loader.patient_ids}
         
         for future in tqdm.tqdm(as_completed(futures), total=len(data_loader.patient_ids)):
